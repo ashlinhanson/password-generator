ddaf973e51dc75e162540061e04ff998f3200be1
ashlinhanson <ashlinhanson@gmail.com>
2020-08-03 11:36:27 -0500

    generator value functions

--- script.js
+++ script.js
@@ -12,3 +12,26 @@ function writePassword() {
 
 // Add event listener to generate button
 generateBtn.addEventListener("click", writePassword);
+
+
+// generator functions - http://www.net-comber.com/charset.html
+
+
+
+function getLower() {
+    const lowerCase = 'abcdefghijklmnopqrstuvwxyz'
+    return lowerCase[Math.floor(Math.random() * lowerCase.length)];
+}
+
+function getUpper() {
+    const upperCase = 'ABCDEFGHIJKLMNOPQRSTUVWXYZ'
+    return upperCase[Math.floor(Math.random() * upperCase.length)];
+
+function getNumber() {
+    const numbers = '0123456789'
+    return numbers[Math.floor(Math.random() * numbers.length)];
+}
+
+function getSymbol() {
+    const symbols = '~!@#$%^&*()+=:;'
+    return symbols[Math.floor(Math.random() * symbols.length)];
